28e306934802711d2b54d3325a702bc9ed92775f
JosephinAngegardien <stephane.issenlor@gmail.com>
2019-07-30 08:45:50 +0200

    Mise à jour

---
+++ src/Controller/VuesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class VuesController extends AbstractController{
+
+    /**
+     * @Route("/", name="pageAccueil")
+     */
+    public function pageAccueil(){
+        return $this->render("pagedaccueil.html.twig");
+    }
+}
+
